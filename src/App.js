38274b34babb541c8ccd63bc24321ff46b09c482
Vlad <vladislav.stan.1998@gmail.com>
2023-06-14 21:56:05 +0300

    added authorization's route

--- src/App.js
+++ src/App.js
@@ -2,12 +2,19 @@ import { Routes, Route } from 'react-router-dom';
 
 import Navigation from './routes/navigation/Navigation';
 import Home from './routes/home/Home';
+import SignIn from './routes/singnIn/SignIn';
+
+const Shop = () => {
+    return <h1>Shop</h1>
+}
 
 const App = () => {
     return (
         <Routes>
             <Route path="/" element={<Navigation />}>
                 <Route index element={<Home />} />
+                <Route path="shop" element={<Shop />} />
+                <Route path="signIn" element={<SignIn />} />
             </Route>
         </Routes>
     );
