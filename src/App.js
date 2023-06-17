704485bc8fbcf6be7cdbc07058b60956f4d37e34
Vlad <vladislav.stan.1998@gmail.com>
2023-06-17 09:17:09 +0300

    replaced authentication path

--- src/App.js
+++ src/App.js
@@ -2,19 +2,19 @@ import { Routes, Route } from 'react-router-dom';
 
 import Navigation from './routes/navigation/Navigation';
 import Home from './routes/home/Home';
-import SignIn from './routes/singnIn/SignIn';
+import Authentication from './routes/authentication/Authentication';
 
 const Shop = () => {
-    return <h1>Shop</h1>
-}
+    return <h1><Shop></Shop></h1>;
+};
 
 const App = () => {
     return (
         <Routes>
-            <Route path="/" element={<Navigation />}>
+            <Route path='/' element={<Navigation />}>
                 <Route index element={<Home />} />
-                <Route path="shop" element={<Shop />} />
-                <Route path="signIn" element={<SignIn />} />
+                <Route path='shop' element={<Shop />} />
+                <Route path='auth' element={<Authentication />} />
             </Route>
         </Routes>
     );
