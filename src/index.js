51f26f9a5873f81a8b4d7a2d2cab7551c3948484
Vlad <vladislav.stan.1998@gmail.com>
2023-06-18 16:42:25 +0300

    wrapped app in ProductsProvider context

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
 import App from './App';
-import { UserProvider } from './context/userContext';
+import { UserProvider } from './context/UserContext';
+import { ProductsProvider } from './context/ProductsData';
 import reportWebVitals from './reportWebVitals';
 
 import './index.scss';
@@ -13,7 +14,9 @@ root.render(
     <React.StrictMode>
         <BrowserRouter>
             <UserProvider>
-                <App />
+                <ProductsProvider>
+                    <App />
+                </ProductsProvider>
             </UserProvider>
         </BrowserRouter>
     </React.StrictMode>,
